bbd0db047e674ec2990b66bf939975880259db7a
THANHHUNG <THANHHUNG@DESKTOP-496TKQG>
2023-08-30 08:37:42 +0700

    Add project files.

---
+++ buoi_2/Bai1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi_2
+{
+    class bai1
+    {
+        private int x;
+        private int y;
+        private string name;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public bai1()
+        {
+            x = 0;
+            y = 0;
+            name = "Origin";
+        }
+
+        public bai1(int x, int y, string name)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} ({x}, {y})";
+        }
+    }
+
+}
---
+++ buoi_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace buoi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bai1 b1 = new bai1();
+            bai1 origin = new bai1();
+            bai1 point = new bai1(5, 10, "Point");
+            Console.WriteLine("Origin: " + origin);
+            Console.WriteLine("Point: " + point);
+        }
+    }
+}
